687c6c5fadbef0c10f613a3dcee37c5b86d0025e
hatealxh <aatsou12@gmail.com>
2023-02-20 03:17:40 -0800

    last code

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stsio.h>
+#include <unistd>
+/**
+ * main - main
+ *
+ * Return: 1 on success
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	return (1);
+}
