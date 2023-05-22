be3d46d451370169a88b31d31e14d33d67dd172c
Abel <aatsou12@gmail.com>
2023-05-22 07:29:28 -0700

    one to nine

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,25 +7,17 @@
 
 int main(void)
 {
-	int j;
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		for (j = 0 ; j < 10 ; j++)
+		putchar(i + '0');
+		if (i != 9)
 		{
-			if (i < j && i != 17)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if (i + j != 17)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-
+			putchar(',');
+			putchar(' ');
 		}
+
 	}
 	putchar('\n');
 	return (0);
