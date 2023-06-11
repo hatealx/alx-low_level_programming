b70df9a191c9678a107bbd1caa5fe48516f112e7
Abel <aatsou12@gmail.com>
2023-06-11 12:24:16 -0700

    creating a static library

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strpbrk - finds first matching char in string
+ * @s: input string to search for matching char
+ * @accept: characters that could be matched
+ * Return: pointer to matching char
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	
+}
+
