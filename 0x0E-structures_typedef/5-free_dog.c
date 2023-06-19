06158a45fb7ebd43d6bda5ff8aaef15ef3e9d177
Abel <aatsou12@gmail.com>
2023-06-19 13:42:50 -0700

    lets free thedog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees dogs
+ * @d: pointer to dog to free
+ *
+ * Return: void
+ */
+void free_dog(dog_t *dg)
+{
+	if (dg)
+	{
+		free(dg->name);
+		free(dg->owner);
+		free(dg);
+	}
+}
