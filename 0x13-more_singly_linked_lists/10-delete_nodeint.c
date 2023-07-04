d89325f24c74183d30c45efdbe75efb245f73bec
Abel <aatsou12@gmail.com>
2023-07-04 06:43:01 -0700

    done

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		next = (*head)->next;
 		free(*head);
-		*head = nex;
+		*head = next;
 		return (1);
 	}
 	current = *head;
